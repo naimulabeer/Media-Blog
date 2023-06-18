52a2f6f2b8edc8ab38944e887caed3da5e213865
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-19 03:19:50 +0600

    adding a few components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import UsersList from "./components/UsersList";
+
 function App() {
-  return <div>Media Blog</div>;
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
