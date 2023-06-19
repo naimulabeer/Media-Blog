4079993bb9ea189fe57b6fe2732c69e1ca245d95
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-19 21:01:09 +0600

    4th steps of adding thunk

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
@@ -7,7 +8,19 @@ const usersSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
