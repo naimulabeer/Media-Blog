fc187a020206a2647138f23b575f489e1cc43b3d
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-19 21:14:20 +0600

    wrapping up the thunks

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return "Users List";
 }
 
