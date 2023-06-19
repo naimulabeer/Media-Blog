090ff171e1bfdedaed33947f250b8ee5104bc39c
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-19 22:07:36 +0600

    using loading state

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users; ///// {data: [], isLoading: false, error: null}
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return "Users List";
+  if (isLoading) {
+    return <div>Loading!!</div>;
+  }
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
