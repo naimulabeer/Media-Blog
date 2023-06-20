8e207e7e61e622766b93dd7c2e49623f7bffecdc
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-20 20:21:21 +0600

    adding the expandablePanel

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,7 @@ import { BsFillTrashFill } from "react-icons/bs";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,19 +11,17 @@ function UsersListItem({ user }) {
     doRemoveUser(user);
   };
 
-  return (
-    <div className="mb-2 border-rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between ">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <BsFillTrashFill />
-          </Button>
-          {error && <div>Error Deleting User</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <BsFillTrashFill />
+      </Button>
+      {error && <div>Error Deleting User</div>}
+      {user.name}
+    </>
   );
+
+  return <ExpandablePanel header={header}>CONTENT!!!</ExpandablePanel>;
 }
 
 export default UsersListItem;
