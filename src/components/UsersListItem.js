594b0c862089177a5bb72669fa60c2f08cedff3c
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-20 19:25:28 +0600

    deleting the user

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,7 +1,22 @@
+import { BsFillTrashFill } from "react-icons/bs";
+import Button from "./Button";
+import { removeUser } from "../store";
+import { useThunk } from "../hooks/use-thunk";
+
 function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
   return (
     <div className="mb-2 border-rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
+        <Button loading={isLoading} onClick={handleClick}>
+          <BsFillTrashFill />
+        </Button>
+        {error && <div>Error Deleting User</div>}
         {user.name}
       </div>
     </div>
