fdc0c0e973ca650b9644308af68da0e43d2c75bb
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-21 19:24:00 +0600

    refetching with tags

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -9,6 +9,7 @@ const albumsApi = createApi({
   endpoints(builder) {
     return {
       addAlbum: builder.mutation({
+        invalidatesTags: ["Album"],
         query: (user) => {
           return {
             url: "/albums",
@@ -21,6 +22,7 @@ const albumsApi = createApi({
         },
       }),
       fetchAlbums: builder.query({
+        providesTags: ["Album"],
         query: (user) => {
           return {
             url: "/albums",
