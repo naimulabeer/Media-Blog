a65df77a15aafb92c228e1dee7dedb0668315dff
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-22 00:29:52 +0600

    easy tag invalidation

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -21,6 +21,9 @@ const albumsApi = createApi({
   endpoints(builder) {
     return {
       removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.userId }];
+        },
         query: (album) => {
           return {
             url: `/albums/${album.id}`,
