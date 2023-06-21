e4dfe15ce2cefe9f27ae4238600d749e4f1e71e6
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-21 20:43:24 +0600

    implementing delete endpoint

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,8 @@
-import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from "../store";
 import Skeleton from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
@@ -6,6 +10,7 @@ import Button from "./Button";
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
+  const [removeAlbum, removeAlbumResults] = useRemoveAlbumMutation();
 
   const handleAddAlbum = () => {
     addAlbum(user); ///user I want to tie my album to.//
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -20,6 +20,14 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
           return [{ type: "Album", id: user.id }];
@@ -53,6 +61,10 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 
 export { albumsApi };
